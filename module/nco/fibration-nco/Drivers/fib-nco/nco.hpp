702ce14a6ba5b29576f99481e303634f27288fc3
therram <lukas.neverauskis@gmail.com>
2020-07-26 17:19:07 +0300

    added classes.

--- module/nco/fibration-nco/Drivers/fib-nco/nco.hpp
+++ module/nco/fibration-nco/Drivers/fib-nco/nco.hpp
@@ -1,13 +1,32 @@
 /*
- * nco.h
- *
- *  Created on: Jul 26, 2020
- *      Author: Lukas Neverauskis
- */
+    Numerically-controlled oscillator for project Fibration.
+    Copyright (C) 2020 Lukas Neverauskis
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #ifndef FIB_NCO_NCO_HPP_
 #define FIB_NCO_NCO_HPP_
 
+#include "hwg.hpp"
+
+class NCO : public HWG {
+
+
+
+
+};
 
 
 #endif /* FIB_NCO_NCO_HPP_ */
