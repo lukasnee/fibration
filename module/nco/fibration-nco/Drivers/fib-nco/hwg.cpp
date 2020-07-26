21f118eff0b3a8f6fd010a2ed0fccfc4bb4d3d24
therram <lukas.neverauskis@gmail.com>
2020-07-26 17:09:11 +0300

    created hardware "glue" and nco source files.

---
+++ module/nco/fibration-nco/Drivers/fib-nco/hwg.cpp
@@ -0,0 +1,8 @@
+/*
+ * hwg.cpp
+ *
+ *  Created on: Jul 26, 2020
+ *      Author: Lukas Neverauskis
+ */
+
+
---
+++ module/nco/fibration-nco/Drivers/fib-nco/nco.hpp
@@ -0,0 +1,13 @@
+/*
+ * nco.h
+ *
+ *  Created on: Jul 26, 2020
+ *      Author: Lukas Neverauskis
+ */
+
+#ifndef FIB_NCO_NCO_HPP_
+#define FIB_NCO_NCO_HPP_
+
+
+
+#endif /* FIB_NCO_NCO_HPP_ */
