930c6a2f4bd39e25f881c1df4bceca08772538dc
lukasnee <lukas.neverauskis@gmail.com>
2020-11-28 00:08:28 +0200

    updated log: added verbosity leveling, timestamp

--- System/log.cpp
+++ System/log.cpp
@@ -1,80 +1,95 @@
 #include "log.hpp"
+#include "system.hpp"
 
 #include <string>
 #include <cstdio>
 #include <cstdarg>
 
-void Log::error(const std::string &context, const char *fmt, ... )
+void Log::error(const std::string &context, const std::string &fmt, ... )
 {
 	if(LOG_ENABLED)
 	{
 		va_list arglist;
 		va_start(arglist, fmt);
-		log(LOG_TYPE_ERROR, context, fmt, arglist);
+		log(VERBOSITY_0, LOG_TYPE_ERROR, context, fmt, arglist);
 		va_end(arglist);
 	}
 };
 
-void Log::warning(const std::string &context, const char *fmt, ... )
+void Log::warning(const std::string &context, const std::string &fmt, ... )
 {
 	if(LOG_ENABLED)
 	{
 		va_list arglist;
 		va_start(arglist, fmt);	
-		log(LOG_TYPE_WARNING, context, fmt, arglist);
+		log(VERBOSITY_0, LOG_TYPE_WARNING, context, fmt, arglist);
 		va_end(arglist);
 	}
 };
 
-void Log::message(const std::string &context, const char *fmt, ... )
+void Log::info(const std::string &context, const std::string &fmt, ... )
 {
 	if(LOG_ENABLED)
 	{
 		va_list arglist;
 		va_start(arglist, fmt);	
-		log(LOG_TYPE_MESSAGE, context, fmt, arglist);
+		log(VERBOSITY_0, LOG_TYPE_INFO, context, fmt, arglist);
 		va_end(arglist);
 	}
 };
 
-void Log::syserr(const std::string &context, const char *fmt, ... )
+void Log::info(Verbosity verbosity, 
+	const std::string &context, const std::string &fmt, ... )
+{
+	if(LOG_ENABLED)
+	{
+		va_list arglist;
+		va_start(arglist, fmt);
+		log(verbosity, LOG_TYPE_INFO, context, fmt, arglist);
+		va_end(arglist);
+	}
+}
+
+void Log::syserr(const std::string &context, const std::string &fmt, ... )
 {
 	if(LOG_ENABLED)
 	{
 		va_list arglist;
 		va_start(arglist, fmt);	
-		log(LOG_TYPE_SYSERR, context, fmt, arglist);
+		log(VERBOSITY_0, LOG_TYPE_SYSERR, context, fmt, arglist);
 		va_end(arglist);
 	}
 };
 
-void Log::log(Type type, 
-	const std::string &context, const char *fmt, va_list arglist)
+void Log::log(Verbosity verbosity, 
+	Type type, const std::string &context, const std::string &fmt, va_list arglist)
 {
 	if(LOG_ENABLED)
 	{
-		// todo real timeStamp
-		const std::string timeStamp = "0.123"; // ms	
 
-		enum ColorType { COLOR, TYPE, COLORTYPE_SIZE };
-		// try just char pointer array... maybe more efficient?
-		const std::string arColorAndType[LOG_TYPES_SIZE][COLORTYPE_SIZE] = {
-			{"\e[39m", "SYSERR"}, // white
-			{"\e[31m", "ERROR"}, // red
-			{"\e[33m", "WARNING"}, // yellow
-			{"\e[36m", "MESSAGE"}, // cyan
-		};
+		if(verbosity <= LOG_VERBOSITY_LEVEL)
+		{
+			// try just char pointer array... maybe more efficient?
+			const std::string arLogType[LOG_TYPES_SIZE] = {
+				{"\e[39mSYSERR"}, 	// white
+				{"\e[31mERROR"}, 	// red
+				{"\e[33mWARNING"}, 	// yellow
+				{"\e[36mINFO"}, 	// cyan
+			};
 
-		/* todo could be very inefficient using std::string (memory and 
-		perfomance wise), test this. maybe try https://github.com/fmtlib/fmt 
-		for printig and formatting */
-		printf("%s%s\t%s\t%s\t", 
-			arColorAndType[type][COLOR].c_str(),
-			timeStamp.c_str(), 
-			arColorAndType[type][TYPE].c_str(), 
-			context.c_str());
-			
-		vprintf(fmt, arglist);
-		printf("\e[0m\n\r");
+			uint32_t logTick = System::getTick();
+
+			/* todo could be very inefficient using std::string (memory and 
+			perfomance wise), test this. maybe try https://github.com/fmtlib/fmt 
+			for printig and formatting */
+			printf("%02lu:%02lu.%03lu\t%s\e[0m\t%s\t",
+				logTick/1000/60, // minutes 
+				logTick/1000%60, // seconds
+				System::getTick()%1000, // milliseconds
+				arLogType[type].c_str(),
+				context.c_str());
+			vprintf(fmt.c_str(), arglist);
+			printf("\n\r");
+		}
 	}
 };
--- System/log.hpp
+++ System/log.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-#define LOG_ENABLED true
+#define LOG_ENABLED 				true
+#define LOG_VERBOSITY_LEVEL 		Log::VERBOSITY_2
 
 #include <string>
 #include <cstdarg>
@@ -8,12 +9,21 @@
 class Log
 {
 public:
-	static void error(const std::string &context, const char *fmt, ... );
-	static void warning(const std::string &context, const char *fmt, ... );
-	static void message(const std::string &context, const char *fmt, ... );
+	static void error(const std::string &context, const std::string &fmt, ... );
+	static void warning(const std::string &context, const std::string &fmt, ... );
+	static void info(const std::string &context, const std::string &fmt, ... );
+
+	enum Verbosity
+	{
+		VERBOSITY_0,
+		VERBOSITY_1,
+		VERBOSITY_2,
+	};
+
+	static void info(Verbosity verbosity, const std::string &context, const std::string &fmt, ... );
 
 protected:
-	static void syserr(const std::string &context, const char *fmt, ... );
+	static void syserr(const std::string &context, const std::string &fmt, ... );
 
 private:	
 	typedef enum Type_ 
@@ -21,10 +31,10 @@ private:
 		LOG_TYPE_SYSERR,
 		LOG_TYPE_ERROR,
 		LOG_TYPE_WARNING,
-		LOG_TYPE_MESSAGE,
+		LOG_TYPE_INFO,
 	LOG_TYPES_SIZE} Type;
 	
-	static void log(Type type, 
-		const std::string &context, const char *fmt, va_list arglist);
+static void log(Verbosity verbosity, 
+	Type type, const std::string &context, const std::string &fmt, va_list arglist);
 
 };
