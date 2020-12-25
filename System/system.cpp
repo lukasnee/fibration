4e52f49993efc771fdb51ca9c5750587b036fa62
lukasnee <lukas.neverauskis@gmail.com>
2020-12-26 01:20:43 +0200

    indentations with spaces.

--- System/system.cpp
+++ System/system.cpp
@@ -9,7 +9,7 @@
 
 int main()
 {
-	FibSys::run(); // never returns
+    FibSys::run(); // never returns
 }
 
 #define STDALLOC true
@@ -45,54 +45,54 @@ static void Error_Handler()
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-	if (htim->Instance == TIM7)
-	{
-		HAL_IncTick();
-	}
+    if (htim->Instance == TIM7)
+    {
+        HAL_IncTick();
+    }
 }
 
 static void SystemClock_Config(void)
 {
-	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
-	RCC_PeriphCLKInitTypeDef PeriphClkInit = { 0 };
+    RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+    RCC_PeriphCLKInitTypeDef PeriphClkInit = { 0 };
 
-	/* Initializes the RCC Oscillators according to the specified parameters
-	in the RCC_OscInitTypeDef structure. */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-	RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-	{
-		Error_Handler();
-	}
-	/* Initializes the CPU, AHB and APB buses clocks */
-	RCC_ClkInitStruct.ClockType =
-		RCC_CLOCKTYPE_HCLK |
-		RCC_CLOCKTYPE_SYSCLK |
-		RCC_CLOCKTYPE_PCLK1 |
-		RCC_CLOCKTYPE_PCLK2;
+    /* Initializes the RCC Oscillators according to the specified parameters
+    in the RCC_OscInitTypeDef structure. */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /* Initializes the CPU, AHB and APB buses clocks */
+    RCC_ClkInitStruct.ClockType =
+        RCC_CLOCKTYPE_HCLK |
+        RCC_CLOCKTYPE_SYSCLK |
+        RCC_CLOCKTYPE_PCLK1 |
+        RCC_CLOCKTYPE_PCLK2;
 
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-	{
-		Error_Handler();
-	}
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+    {
+        Error_Handler();
+    }
 
-	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
-	PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK2;
-	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-	{
-		Error_Handler();
-	}
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
+    PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK2;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+    {
+        Error_Handler();
+    }
 }
 
 size_t FibSys::initialFreeHeapSize = 0;
@@ -124,29 +124,29 @@ void FibSys::error()
 
 void FibSys::initTasks(UBaseType_t priority)
 {
-	BaseType_t xReturned;
+    BaseType_t xReturned;
     xReturned = xTaskCreate(FibSys::vSystemTask, "system", 0x200, NULL, priority, &FibSys::hSystemTask);	
-	if(xReturned == pdFAIL) 
-	{
+    if(xReturned == pdFAIL) 
+    {
         FibSys::error();
-	}	
-	
+    }
+    
     xReturned = xTaskCreate(FibSys::vMainTask, "main", 0x200, NULL, priority - 1, &FibSys::hMainTask);		
-	if(xReturned == pdFAIL) 
-	{
+    if(xReturned == pdFAIL) 
+    {
         FibSys::error();
-	}	
+    }
 }
 
 void FibSys::run()
 {
-	const UBaseType_t cSysPriority = configMAX_PRIORITIES - 1;
+    const UBaseType_t cSysPriority = configMAX_PRIORITIES - 1;
     FibSys::initPlatform();
     Log::init();
- 	FibSys::initTasks(cSysPriority);
+     FibSys::initTasks(cSysPriority);
     uwTick = 0; // for nicer log
-	vTaskStartScheduler();
-	while(true); // should never be reached
+    vTaskStartScheduler();
+    while(true); // should never be reached
 }
 
 void FibSys::vSystemTask(void * pvParams)
@@ -155,7 +155,7 @@ void FibSys::vSystemTask(void * pvParams)
     //Log::system("vSystemTask", "initialFreeHeapSize %lu", freeHeapSize);
 
     while(true)
-    {	
+    {
         FibSys::freeHeapSize = xPortGetFreeHeapSize();
         //Log::system("vSystemTask", "freeHeapSize %lu", freeHeapSize);
         FibSys::taskDelay(100);
@@ -171,6 +171,6 @@ void FibSys::vMainTask(void * pvParams)
 
 void FibSys::stop()
 {
-	vTaskDelete(FibSys::hSystemTask);
-	vTaskDelete(FibSys::hMainTask);
+    vTaskDelete(FibSys::hSystemTask);
+    vTaskDelete(FibSys::hMainTask);
 }
