af542f193f5e1bf1abb60be67a26356bcce98ac3
lukasnee <lukas.neverauskis@gmail.com>
2020-12-26 01:20:43 +0200

    indentations with spaces.

--- System/system.hpp
+++ System/system.hpp
@@ -11,24 +11,24 @@
 class FibSys // todo make singleton
 {
 public:
-	static void run();
-	static void stop();
-	static std::uint32_t getTick();
-	static void taskDelay(std::uint32_t ms);
-	static void error();
-	
+    static void run();
+    static void stop();
+    static std::uint32_t getTick();
+    static void taskDelay(std::uint32_t ms);
+    static void error();
+    
 private:
-	static void initPlatform();
-	static void initTasks(std::uint32_t priority);
+    static void initPlatform();
+    static void initTasks(std::uint32_t priority);
 
-	static void vSystemTask(void * pvParams);
-	static void vMainTask(void * pvParams);
+    static void vSystemTask(void * pvParams);
+    static void vMainTask(void * pvParams);
 
-	static TaskHandle_t hSystemTask;
-	static TaskHandle_t hMainTask;
+    static TaskHandle_t hSystemTask;
+    static TaskHandle_t hMainTask;
 
-	static size_t initialFreeHeapSize;
-	static size_t freeHeapSize;
+    static size_t initialFreeHeapSize;
+    static size_t freeHeapSize;
 };
 
 void mainTask(void *); // application entry point
