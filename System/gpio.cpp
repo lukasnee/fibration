93034d2ee8fe18884b0ccd5b93debe74587edb12
lukasnee <lukas.neverauskis@gmail.com>
2021-01-01 14:38:06 +0200

    gpio cleanup.

--- System/gpio.cpp
+++ System/gpio.cpp
@@ -23,128 +23,132 @@
 
 static GPIO_TypeDef * sGetPort(Gpio::Port port)
 {
-	const GPIO_TypeDef * cMap[Gpio::Port::_PORT_ENUM_MAX + 1] = {
-		GPIOA, GPIOB, GPIOC, GPIOD, GPIOE, GPIOF
-	};
-	return const_cast<GPIO_TypeDef *>(cMap[port]);
+    const GPIO_TypeDef * cMap[Gpio::Port::_PORT_ENUM_MAX + 1] = {
+        GPIOA, GPIOB, GPIOC, GPIOD, GPIOE, GPIOF
+    };
+    return const_cast<GPIO_TypeDef *>(cMap[port]);
 }
 
 static uint16_t sGetPin(Gpio::Pin pin)
 {
-	const uint16_t cMap[Gpio::Pin::_PIN_ENUM_MAX + 1] = {
-		GPIO_PIN_0, GPIO_PIN_1, GPIO_PIN_2, GPIO_PIN_3,
-		GPIO_PIN_4, GPIO_PIN_5, GPIO_PIN_6, GPIO_PIN_7,
-		GPIO_PIN_8, GPIO_PIN_9, GPIO_PIN_10, GPIO_PIN_11,
-		GPIO_PIN_12, GPIO_PIN_13, GPIO_PIN_14, GPIO_PIN_15
-	};
-	return cMap[pin];
+    const uint16_t cMap[Gpio::Pin::_PIN_ENUM_MAX + 1] = {
+        GPIO_PIN_0, GPIO_PIN_1, GPIO_PIN_2, GPIO_PIN_3,
+        GPIO_PIN_4, GPIO_PIN_5, GPIO_PIN_6, GPIO_PIN_7,
+        GPIO_PIN_8, GPIO_PIN_9, GPIO_PIN_10, GPIO_PIN_11,
+        GPIO_PIN_12, GPIO_PIN_13, GPIO_PIN_14, GPIO_PIN_15
+    };
+    return cMap[pin];
 }
 
 static uint32_t sGetMode(Gpio::Mode mode)
 {
-	const uint32_t cMap[Gpio::Mode::_MODE_ENUM_MAX + 1] = {
-		GPIO_MODE_INPUT, 
-		GPIO_MODE_OUTPUT_PP, 
-		GPIO_MODE_OUTPUT_OD
-	};
-	return cMap[mode];
+    const uint32_t cMap[Gpio::Mode::_MODE_ENUM_MAX + 1] = {
+        GPIO_MODE_INPUT,
+        GPIO_MODE_OUTPUT_PP,
+        GPIO_MODE_OUTPUT_OD
+    };
+    return cMap[mode];
 }
 
 static uint32_t sGetPull(Gpio::Pull pull)
 {
-	const uint32_t cMap[Gpio::Pull::_PULL_ENUM_MAX + 1] = {
-		GPIO_NOPULL,
-		GPIO_PULLUP,
-		GPIO_PULLDOWN
-	};
-	return cMap[pull];
+    const uint32_t cMap[Gpio::Pull::_PULL_ENUM_MAX + 1] = {
+        GPIO_NOPULL,
+        GPIO_PULLUP,
+        GPIO_PULLDOWN
+    };
+    return cMap[pull];
 }
 
 static uint32_t sGetSpeed(Gpio::Speed speed)
 {
-	const uint32_t cMap[Gpio::Speed::_SPEED_ENUM_MAX + 1] = {
-		GPIO_SPEED_FREQ_LOW,
-		GPIO_SPEED_FREQ_MEDIUM,
-		GPIO_SPEED_FREQ_HIGH
-	};
-	return cMap[speed];
+    const uint32_t cMap[Gpio::Speed::_SPEED_ENUM_MAX + 1] = {
+        GPIO_SPEED_FREQ_LOW,
+        GPIO_SPEED_FREQ_MEDIUM,
+        GPIO_SPEED_FREQ_HIGH
+    };
+    return cMap[speed];
 }
 
 static void sEnableGpioClock(Gpio::Port port)
 {
-	switch(port)
-	{
-		case Gpio::Port::A: __HAL_RCC_GPIOA_CLK_ENABLE(); break;
-		case Gpio::Port::B: __HAL_RCC_GPIOB_CLK_ENABLE(); break;
-		case Gpio::Port::C: __HAL_RCC_GPIOC_CLK_ENABLE(); break;
-		case Gpio::Port::D: __HAL_RCC_GPIOD_CLK_ENABLE(); break;
-		case Gpio::Port::F: __HAL_RCC_GPIOF_CLK_ENABLE(); break;
-		default: break;
-	}
+    switch(port)
+    {
+        case Gpio::Port::A: __HAL_RCC_GPIOA_CLK_ENABLE(); break;
+        case Gpio::Port::B: __HAL_RCC_GPIOB_CLK_ENABLE(); break;
+        case Gpio::Port::C: __HAL_RCC_GPIOC_CLK_ENABLE(); break;
+        case Gpio::Port::D: __HAL_RCC_GPIOD_CLK_ENABLE(); break;
+        case Gpio::Port::F: __HAL_RCC_GPIOF_CLK_ENABLE(); break;
+        default: break;
+    }
 }
 
 static void sGpioInit(
-	Gpio::Port port, 
-	Gpio::Pin pin, 
-	Gpio::Mode mode, 
-	Gpio::Pull pull, 
-	Gpio::Speed speed)
+    Gpio::Port port,
+    Gpio::Pin pin,
+    Gpio::Mode mode,
+    Gpio::Pull pull,
+    Gpio::Speed speed)
 {
-	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
-	GPIO_InitStruct.Pin = sGetPin(pin);
-	GPIO_InitStruct.Mode = sGetMode(mode);
-	GPIO_InitStruct.Pull = sGetPull(pull);
-	GPIO_InitStruct.Speed = sGetSpeed(speed);
-	HAL_GPIO_Init(sGetPort(port), &GPIO_InitStruct);
+    sEnableGpioClock(port);
+
+    GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+    GPIO_InitStruct.Pin = sGetPin(pin);
+    GPIO_InitStruct.Mode = sGetMode(mode);
+    GPIO_InitStruct.Pull = sGetPull(pull);
+    GPIO_InitStruct.Speed = sGetSpeed(speed);
+    HAL_GPIO_Init(sGetPort(port), &GPIO_InitStruct);
 }
 
 
 
 Gpio::Gpio(Port port, Pin pin) : 
-	port(port), 
-	pin(pin),
-	isInitialized(false)
+    port(port),
+    pin(pin),
+    isInitialized(false)
 {
 }
 
-
-void Gpio::init(bool initState, Mode mode, Pull pull, Speed speed)
+void Gpio::init(Mode mode, Pull pull, Speed speed)
 {
-	write(initState);
+    sGpioInit(this->port, this->pin, mode, pull, speed);
+    isInitialized = true;
+}
 
-	sEnableGpioClock(this->port);
-	sGpioInit(this->port, this->pin, mode, pull, speed);
-	isInitialized = true;
+void Gpio::init(Mode mode, LogicState initial, Pull pull, Speed speed)
+{
+    write(initial);
+    init(mode, pull, speed);
 }
 
 
 bool Gpio::read()
 {
-	return (bool)HAL_GPIO_ReadPin(sGetPort(this->port), sGetPin(this->pin));
+    return (bool)HAL_GPIO_ReadPin(sGetPort(this->port), sGetPin(this->pin));
 }
 
 
-void Gpio::write(bool state)
+void Gpio::write(LogicState value)
 {
-	HAL_GPIO_WritePin(sGetPort(this->port), sGetPin(this->pin), 
-		state ? GPIO_PIN_SET : GPIO_PIN_RESET);	
+    HAL_GPIO_WritePin(sGetPort(this->port), sGetPin(this->pin),
+        value == LogicState::low ? GPIO_PIN_RESET : GPIO_PIN_SET);	
 }
 
 
 void Gpio::toggle()
 {
-	HAL_GPIO_TogglePin(sGetPort(this->port), sGetPin(this->pin));
+    HAL_GPIO_TogglePin(sGetPort(this->port), sGetPin(this->pin));
 }
 
 
 void Gpio::deinit()
 {
-	HAL_GPIO_DeInit(sGetPort(this->port), sGetPin(this->pin)); 
-	// note gpio related clock still remain enabled
+    HAL_GPIO_DeInit(sGetPort(this->port), sGetPin(this->pin));
+    // note gpio related clock still remain enabled
 }
 
 
 Gpio::~Gpio()
 {
-	deinit();
+    deinit();
 }
--- System/peripherals.cpp
+++ System/peripherals.cpp
@@ -5,56 +5,56 @@
 
 Uart1 & Periph::getUart1()
 {
-	static Uart1 uart1(10, 115200); return uart1;
+    static Uart1 uart1(10, 115200); return uart1; // todo figure out priority 
 }
 
 Gpio & Periph::getGpio(Gpio::Port port, Gpio::Pin pin)
 {
-	if (port == Gpio::Port::A)
-	{ 
-		if		(pin == Gpio::Pin::P0)  { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P1)  { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P2)  { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P3)  { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P4)  { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P5)  { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P6)  { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P7)  { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P8)  { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P9)  { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P10) { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P11) { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P12) { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P13) { static Gpio gpio(port, pin); return gpio; } // SWIO
-		else if (pin == Gpio::Pin::P14) { static Gpio gpio(port, pin); return gpio; } // SWCLK
-		else if (pin == Gpio::Pin::P15) { static Gpio gpio(port, pin); return gpio; }
-	}
-	if (port == Gpio::Port::B)
-	{ 
-		if		(pin == Gpio::Pin::P0)  { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P1)  { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P2)  { static Gpio gpio(port, pin); return gpio; } // BOOT1
-		else if (pin == Gpio::Pin::P3)  { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P4)  { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P5)  { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P6)  { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P7)  { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P8)  { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P9)  { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P10) { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P11) { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P12) { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P13) { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P14) { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P15) { static Gpio gpio(port, pin); return gpio; }
-	}
-	if (port == Gpio::Port::C)
-	{ 
-		if 		(pin == Gpio::Pin::P13) { static Gpio gpio(port, pin); return gpio; } // RobotDyn LED pin (active low)
-		else if (pin == Gpio::Pin::P14) { static Gpio gpio(port, pin); return gpio; }
-		else if (pin == Gpio::Pin::P15) { static Gpio gpio(port, pin); return gpio; }
-	}
+    if (port == Gpio::Port::A)
+    {
+        if		(pin == Gpio::Pin::pin0)  { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin1)  { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin2)  { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin3)  { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin4)  { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin5)  { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin6)  { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin7)  { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin8)  { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin9)  { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin10) { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin11) { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin12) { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin13) { static Gpio gpio(port, pin); return gpio; } // SWIO
+        else if (pin == Gpio::Pin::pin14) { static Gpio gpio(port, pin); return gpio; } // SWCLK
+        else if (pin == Gpio::Pin::pin15) { static Gpio gpio(port, pin); return gpio; }
+    }
+    else if (port == Gpio::Port::B)
+    {
+        if		(pin == Gpio::Pin::pin0)  { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin1)  { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin2)  { static Gpio gpio(port, pin); return gpio; } // BOOT1
+        else if (pin == Gpio::Pin::pin3)  { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin4)  { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin5)  { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin6)  { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin7)  { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin8)  { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin9)  { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin10) { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin11) { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin12) { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin13) { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin14) { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin15) { static Gpio gpio(port, pin); return gpio; }
+    }
+    else if (port == Gpio::Port::C)
+    {
+        if 		(pin == Gpio::Pin::pin13) { static Gpio gpio(port, pin); return gpio; } // RobotDyn LED pin (active low)
+        else if (pin == Gpio::Pin::pin14) { static Gpio gpio(port, pin); return gpio; }
+        else if (pin == Gpio::Pin::pin15) { static Gpio gpio(port, pin); return gpio; }
+    }
 
-	FibSys::panic(); // never returns...
-	static Gpio dummy(port, pin); return dummy;
+    FibSys::panic(); // never returns...
+    static Gpio dummy(port, pin); return dummy;
 }
