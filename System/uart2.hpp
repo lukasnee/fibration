bef9e896e2c062f2315abe20b0e895335db7e4cf
lukasnee <lukas.neverauskis@gmail.com>
2021-01-26 23:44:10 +0200

    Made uart2 based on Stream.

--- System/uart2.hpp
+++ System/uart2.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
+#include "stream.hpp"
 #include <cstdint>
 
-class  Uart2 {
+class  Uart2 : public Stream {
 public:
     Uart2(std::uint32_t baudrate);
 
-    void putc(char c);
-    char getc(void);
+    void putc(char c) override;
+    char getc(void) override;
 
     void operator=(Uart2 const&)  = delete;
     Uart2(Uart2 const&) = delete;
