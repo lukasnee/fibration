69b3e7d1f5f409cfbafd15d0cc766b84d158d0b3
lukasnee <lukas.neverauskis@gmail.com>
2021-01-26 23:43:37 +0200

    Stream interface.

--- System/stream.hpp
+++ System/stream.hpp
@@ -1,11 +1,7 @@
 #pragma once
 
-namespace Fib {
-
 class Stream {
-	// todo
-	virtual bool write(std::uint8_t *pData, std::uint16_t size) = 0;
-	virtual bool read(std::uint8_t *pData, std::uint16_t size) = 0;
+public:
+    virtual void putc(char c) = 0;
+    virtual char getc(void) = 0;
 };
-
-} // namespace Fib
