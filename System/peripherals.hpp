547de0a1d574a65f3b16f2ddfa0b707ec9ccdf32
lukasnee <lukas.neverauskis@gmail.com>
2021-01-26 23:44:27 +0200

    added uart2 to peripherals.

--- System/peripherals.hpp
+++ System/peripherals.hpp
@@ -2,6 +2,7 @@
 
 #include "tim6.hpp" 
 #include "uart1.hpp" 
+#include "uart2.hpp" 
 #include "gpio.hpp"
 
 class Periph 
@@ -9,5 +10,6 @@ class Periph
 public:
 	static Tim6 & getTim6();
 	static Uart1 & getUart1();
+	static Uart2 & getUart2();
 	static Gpio & getGpio(Gpio::Port port, Gpio::Pin pin);
 };
