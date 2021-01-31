6a62b8e400ef42e4efcd97318c7692f347f7fb1c
lukasnee <lukas.neverauskis@gmail.com>
2021-01-31 21:43:27 +0200

    system status and config initial.

---
+++ System/status.hpp
@@ -0,0 +1,15 @@
+
+#include <utility>
+
+class Stats {
+public:
+    using KeyType = const char *;
+    template<typename ValueType> using KeyValue = std::pair<KeyType, ValueType>;
+
+
+    struct Sysinfo {
+        KeyValue<std::uint32_t> curfreeHeapSize;
+        KeyValue<std::uint32_t> minFreeHeapSize;
+        KeyValue<std::uint32_t> maxFreeHeapSize;
+    } sysInfo;
+};
