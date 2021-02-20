b16583475f6149b1be0fb2a6b935e36718eeee34
lukasnee <lukas.neverauskis@gmail.com>
2021-02-20 20:19:10 +0200

    Shell char processing reworked, pretty robust. Now you can move cursor left/right, insert char and backspace from anywhere.

--- System/shell.cpp
+++ System/shell.cpp
@@ -1,5 +1,5 @@
 #include "shell.hpp"
-
+#include "log.hpp"
 #include "system.hpp"
 #include "peripherals.hpp"
 
@@ -44,28 +44,52 @@ void Shell::Run()
     this->echoEndLine();
     this->echoLine("Type 'help' to list all commands");
     this->promptNew();
-    
+
     // Consume and send characters to shell...forever
     while (true)
     {
-        this->receiveChar(this->stream.getc());
+        // NOTE: escape sequences are time sensitive !
+        if(this->receiveChar(this->stream.getc()))
+        { // escape sequence finished (not time sensitive)
+            // Log::trace("shell", "c:%u t:%u %s", this->rxCursorIdx, this->rxCharsTotal, this->rxBuffer.data());
+        }
     }
 }
 
-void Shell::echo(char c)
+void Shell::echo(char c, std::size_t timesToRepeat)
 {
-    if (c == '\n')
+    while (timesToRepeat--)
     {
-        this->stream.putc('\r');
+        if (c == '\n')
+        {
+            this->stream.putc('\r');
+        }
+        this->stream.putc(c);
     }
-    this->stream.putc(c);
 }
 
-void Shell::echo(const char *string)
+void Shell::echo(const char *string, std::size_t timesToRepeat)
 {
-    for (const char *c = string; *c != '\0'; ++c)
+    while (timesToRepeat--)
     {
-        this->echo(*c);
+        for (const char *c = string; *c != '\0'; ++c)
+        {
+            this->echo(*c);
+        }
+    }
+}
+
+void Shell::echoData(const char *pData, const std::size_t len, std::size_t timesToRepeat)
+{
+    while (timesToRepeat--)
+    {
+        std::size_t lenIt = len;
+        const char * pDataIt = pData;
+        
+        while (lenIt--)
+        {
+            this->echo(*(pDataIt++));
+        }
     }
 }
 
@@ -85,8 +109,8 @@ void Shell::echoEndLine()
 void Shell::resetRxBuffer(void)
 {
     this->rxBuffer.fill('\0');
-    this->rxHead = 0;
-    this->rxSize = 0;
+    this->rxCharsTotal = 1;
+    this->rxCursorIdx = 0;
 }
 
 void Shell::promptNew(void)
@@ -100,122 +124,311 @@ void Shell::printPrompt(void)
     this->echo(Shell::prompt.data());
 }
 
-void Shell::receiveChar(char c/*, char c2*/)
+bool Shell::rxBufferCursorOnBase()
 {
-    if (c == '\r') // end of line
+    return (this->rxCursorIdx == 0);
+}
+
+bool Shell::rxBufferCursorNotOnBase()
+{
+    return (false == this->rxBufferCursorOnBase());
+}
+
+bool Shell::rxBufferCursorOnTail()
+{
+    return (this->rxCharsTotal - this->rxCursorIdx == 1);
+}
+
+bool Shell::rxBufferCursorNotOnTail()
+{
+    return (false == this->rxBufferCursorOnTail());
+}
+
+bool Shell::rxBufferIsEmpty()
+{
+    return (this->rxBufferCursorOnBase() and this->rxBufferCursorOnTail());
+}
+
+bool Shell::rxBufferIsNotEmpty()
+{
+    return (false == this->rxBufferIsEmpty());
+}
+
+bool Shell::rxBufferIsFull()
+{
+    return (this->rxCharsTotal >= this->rxBuffer.size());
+}
+
+bool Shell::rxBufferIsNotFull()
+{
+    return (false == this->rxBufferIsFull());
+}
+
+void Shell::visualBackspace()
+{
+    this->echo('\b');
+}
+
+bool Shell::rxBufferCursorStep()
+{
+    if(this->rxBufferIsNotFull())
     {
-        if (this->isRxBufferFull() == false)
-        {
-            this->rxBuffer[this->rxHead++] = '\n';
-            this->rxSize++;
-        }
-        this->echoEndLine();
+        this->rxCursorIdx++;
+        return true;
     }
-    else if (c == '\b') // backspace
+    return false;
+}
+
+bool Shell::rxBufferCursorStepBack()
+{
+    if(this->rxBufferCursorNotOnBase())
     {
-        if (this->rxHead)
-        {
-            this->rxHead--;
-            this->rxSize--;
-            this->rxBuffer[this->rxSize] = '\0';
-            this->echo("\b \b");
-        }
-        return;vTaskSuspend(nullptr);
+        this->rxCursorIdx--;
+        return true;
     }
-    else if (c == '['/* && c2 == 'D'*/)
+    return false;
+}
+
+char Shell::rxBufferCharOnCursor()
+{
+    return this->rxBuffer.at(this->rxCursorIdx);
+}
+
+char Shell::rxBufferLastChar()
+{
+    if(this->rxBufferIsNotEmpty())
     {
-        rxHead--;
-        this->echo('\b');
-        return;
-    }
-    else if (c >= 0x20) // ascii text symbols
-    {
-        if (this->isRxBufferFull() == false)
-        {
-            if (this->rxHead == this->rxSize)
-            { // appending line
-                this->rxSize++;
-            }
-            this->rxBuffer[this->rxHead++] = c;
-            this->echo(c);
-        }
+        return this->rxBuffer.at(this->rxCharsTotal - 1 - sizeof('\0'));
     }
     else
     {
-        return;
+        return '\0';
     }
-
-    this->process();
 }
 
-void Shell::process(void)
+// ESC[H	moves cursor to home position (0, 0)
+// ESC[{line};{column}H
+// ESC[{line};{column}f	moves cursor to line #, column #
+// ESC[#A	moves cursor up # lines
+// ESC[#B	moves cursor down # lines
+// ESC[#C	moves cursor right # columns
+// ESC[#D	moves cursor left # columns
+// ESC[#E	moves cursor to beginning of next line, # lines down
+// ESC[#F	moves cursor to beginning of previous line, # lines down
+// ESC[#G	moves cursor to column #
+// ESC[#;#R	reports current cursor line and column
+// ESC[s	saves the current cursor position
+// ESC[u	restores the cursor to the last saved position
+
+// returns true if sequence finished
+bool Shell::processAnsiCursorControl(char c)
 {
-    if (this->lastChar() == '\n')
+    if (c == 'H') // home position
     {
-        std::array<char *, Shell::maxArgs> argv{{0}};
-        std::size_t argc = 0;
-
-        char *nextArg = nullptr;
-        for (std::size_t i = 0; i < this->rxSize && argc < argv.size(); ++i)
+        while (this->rxBufferCursorStepBack())
         {
-            char *const c = &this->rxBuffer[i];
-            if (*c == ' ' || *c == '\n' || i == this->rxSize - 1)
-            {
-                *c = '\0';
-                if (nextArg)
-                {
-                    argv[argc++] = nextArg;
-                    nextArg = nullptr;
-                }
-            }
-            else if (!nextArg)
-            {
-                nextArg = c;
-            }
+            this->visualBackspace();
         }
-
-        if (argc >= 1)
-        {
-            const Shell::Command *command = findCommand(argv[0]);
-            if (!command)
-            {
-                this->printf("\e[39mfibration: %s: command not found ", argv[0]);
-                this->echoLine(argv[0]);
-            }
-            else
-            {
-                echo("\e[32m"); // response in green
-                int result = command->handler(*this, argc, argv.data());
-                if(ShellConfig::regularResponseIsEnabled)
-                {
-                    if (result == 0)
-                    {
-                        this->echoLine("\e[32mOK"); // green
-                    }
-                    else if (result < 0)
-                    {
-                        this->printf("\e[31mFAIL: %d\n", result); // red
-                    }
-                    else if (result == 1) // no echo
-                    {
-                    }
-                }
-            }
-        }
-
-        this->resetRxBuffer();
-        this->printPrompt();
+        return true;
     }
+    else if (c == 'A') // cursor up
+    {
+        /* commands scrolling unsupported */
+        return true;
+    }
+    else if (c == 'B') // cursor down
+    {
+        /* commands scrolling unsupported */
+        return true;
+    }
+    else if (c == 'C') // cursor right
+    {
+        if (this->rxBufferCursorNotOnTail())
+        {
+            // ovewrite char with same value effectively moving cursor right
+            this->echo(this->rxBufferCharOnCursor());
+            this->rxBufferCursorStep();
+        }
+        return true;
+    }
+    else if (c == 'D') // cursor left
+    {
+        if (this->rxBufferCursorNotOnBase())
+        {
+            this->visualBackspace();
+            this->rxBufferCursorStepBack();
+        }
+        return true;
+    }
+
+    return true;
 }
 
-char Shell::lastChar(void)
+// returns true if sequence finished
+bool Shell::processAnsiEscapeSequences(char c)
 {
-    return this->rxBuffer.at(this->rxSize - 1);
+    static bool delimited = false;
+
+    if (c == '[') // open delimiter
+    {
+        delimited = true;
+    }
+    else if (delimited) // proccess command
+    {
+        if (this->processAnsiCursorControl(c))
+        {
+            delimited = false; // close delimiter
+            return true;
+        }
+    }
+
+    return false;
 }
 
-bool Shell::isRxBufferFull(void)
+// returns false if escape sequence in progress (time sensitive)
+bool Shell::receiveChar(char c)
 {
-    return (this->rxSize >= Shell::rxBufferSize);
+    static bool escaped = false;
+
+    if (c == '\e') // escaped
+    {
+        escaped = true;
+        return false;
+    }
+    else if (escaped)
+    {
+        if (c == 0x7F) // DEL(ete)
+        {
+            if (this->rxBufferIsNotEmpty() && this->rxBufferCursorNotOnTail())
+            {
+                std::memmove(&this->rxBuffer[this->rxCursorIdx], &this->rxBuffer[this->rxCursorIdx + 1], this->rxCharsTotal - (this->rxCursorIdx + 1));
+                this->rxBuffer[--this->rxCharsTotal] = '\0';
+
+                this->echoData(&this->rxBuffer[this->rxCursorIdx], this->rxCharsTotal - this->rxCursorIdx);
+                this->echo(' ');
+                this->echo('\b', this->rxCharsTotal - this->rxCursorIdx + 1);
+            }
+            escaped = false;
+        }
+        else if (this->processAnsiEscapeSequences(c))
+        {
+            escaped = false;
+        }
+        
+        return false;
+    }
+    else if (c == '\r') // enter!
+    {
+        this->echoEndLine();
+        this->processRxBufferIntoArgs();
+        return true;
+    }
+    else if (c == '\b') // backspace
+    {
+        if (this->rxBufferCursorNotOnBase())
+        {
+            // update buffer
+            std::memmove(&this->rxBuffer[this->rxCursorIdx - 1], &this->rxBuffer.at(this->rxCursorIdx), this->rxCharsTotal - this->rxCursorIdx);
+            if(this->rxBufferCursorStepBack())
+            {
+                this->rxBuffer[--this->rxCharsTotal] = '\0';
+
+                // adjust visuals accordingly
+                this->visualBackspace();
+                this->echoData(&this->rxBuffer[this->rxCursorIdx], this->rxCharsTotal - (this->rxCursorIdx));
+                this->echo("  ");
+                this->echo('\b', this->rxCharsTotal - (this->rxCursorIdx) + 1);
+            }
+        }
+        
+        return true;
+    }
+    else if (c >= 0x20 && c <= 0x7E) // ascii text symbols
+    {
+        if (this->rxBufferIsNotFull())
+        {
+            if (this->rxBufferCursorNotOnTail()) 
+            {
+                std::memmove(&this->rxBuffer[this->rxCursorIdx + 1], 
+                &this->rxBuffer.at(this->rxCursorIdx), 
+                this->rxCharsTotal - this->rxCursorIdx);
+            }
+
+            this->rxBuffer[this->rxCursorIdx] = c;
+            this->rxBufferCursorStep();
+            this->rxBuffer[this->rxCharsTotal++] = '\0';
+
+            if (this->rxBufferCursorOnTail()) // then append char
+            {
+                this->echo(c);
+            }
+            else // insert within line
+            {
+                this->echo(&this->rxBuffer[this->rxCursorIdx - 1]);
+                this->echo('\b', this->rxCharsTotal - this->rxCursorIdx - 1);
+            }
+        }
+        return true;
+    }
+    return true;
+}
+
+void Shell::processRxBufferIntoArgs()
+{
+
+    std::array<char *, Shell::maxArgs> argv{{0}};
+    std::size_t argc = 0;
+
+    char *nextArg = nullptr;
+    for (std::size_t i = 0; i < this->rxCharsTotal && argc < argv.size(); ++i)
+    {
+        char *const c = &this->rxBuffer[i];
+        if (*c == ' ' || *c == '\0')
+        {
+            *c = '\0';
+            if (nextArg)
+            {
+                argv[argc++] = nextArg;
+                nextArg = nullptr;
+            }
+        }
+        else if (!nextArg)
+        {
+            nextArg = c;
+        }
+    }
+
+    if (argc >= 1)
+    {
+        const Shell::Command *command = findCommand(argv[0]);
+        if (!command)
+        {
+            this->printf("\e[39mfibration: %s: command not found ", argv[0]);
+            this->echoLine(argv[0]);
+        }
+        else
+        {
+            echo("\e[32m"); // response in green
+            int result = command->handler(*this, argc, argv.data());
+            if (ShellConfig::regularResponseIsEnabled)
+            {
+                if (result == 0)
+                {
+                    this->echoLine("\e[32mOK"); // green
+                }
+                else if (result < 0)
+                {
+                    this->printf("\e[31mFAIL: %d\n", result); // red
+                }
+                else if (result == 1) // no echo
+                {
+                }
+            }
+        }
+    }
+
+    this->resetRxBuffer();
+    this->printPrompt();
 }
 
 const Shell::Command *Shell::findCommand(const char *name)
