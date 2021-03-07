cd225a98ae34028f91f7fd60ebc1590046d7e95f
lukasnee <lukas.neverauskis@gmail.com>
2021-03-08 01:02:28 +0200

    More cleanup.

--- System/shellCommands.cpp
+++ System/shellCommands.cpp
@@ -73,3 +73,25 @@ const std::array<Shell::Command, 5> Shell::shellCmds{{
     {"status", "show system status", shellCmdStatus},
     {"panic", "cause system panic", shellPanic},
 }};
+
+// #include <array>
+// #include <string_view>
+// #include <stdexcept>
+// #include <algorithm>
+
+// template <typename Key, typename Value, std::size_t Size>
+// struct Map {
+//   std::array<std::pair<Key, Value>, Size> data;
+
+//   [[nodiscard]] constexpr Value at(const Key &key) const {
+//     const auto itr =
+//         std::find_if(begin(data), end(data),
+//                      [&key](const auto &v) { return v.first == key; });
+//     if (itr != end(data)) {
+//       return itr->second;
+//     } else {
+//       throw std::range_error("Not Found");
+//     }
+//   }
+
+// };
