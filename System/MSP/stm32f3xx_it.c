ae9a5eba0ab9a7e24e65220fe1e0b54eaf4caf88
lukasnee <lukas.neverauskis@gmail.com>
2021-04-10 15:36:33 +0300

    fix: chnage Cortex Fault signal LED to PA5

--- System/MSP/stm32f3xx_it.c
+++ System/MSP/stm32f3xx_it.c
@@ -12,17 +12,15 @@ static void errorLedInit()
     static uint8_t errorLedInitialized = 0;
     if (!errorLedInitialized)
     {
-        __HAL_RCC_GPIOC_CLK_ENABLE();
-        //__HAL_RCC_GPIOF_CLK_ENABLE();
-
+        __HAL_RCC_GPIOA_CLK_ENABLE();
         GPIO_InitTypeDef GPIO_InitStruct = {0};
-        GPIO_InitStruct.Pin = GPIO_PIN_13;
+        GPIO_InitStruct.Pin = GPIO_PIN_5;
         GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
         GPIO_InitStruct.Pull = GPIO_PULLUP;
         GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
-        HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET); // led off
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET); // led off
 
         errorLedInitialized = 1;
     }
@@ -33,9 +31,9 @@ static void blinkErrorLed(uint8_t numberOfTimes)
     errorLedInit();
     while (numberOfTimes--)
     {
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
         HAL_Delay(BLINK_TIME_MS);
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
         HAL_Delay(BLINK_TIME_MS);
     }
     HAL_Delay(OFF_TIME_MS);
