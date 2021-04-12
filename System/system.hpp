6b81804bd9621867ac12415e724a392dcd589185
lukasnee <lukas.neverauskis@gmail.com>
2021-04-13 01:05:15 +0300

    style: tweak fibsys priorities, rename start to boot.

--- System/system.hpp
+++ System/system.hpp
@@ -4,28 +4,31 @@
 #include "status.hpp"
 
 #include <cstdint>
+
 class FibSys : public cpp_freertos::Thread
 {
 public:
     // entry point of fibration system, should be called in main
-    static void start();
+    static void boot();
 
     // 1KHz system tick time reference
     static std::uint32_t getSysTick();
 
     // should be called in case fatal error, could be used as run-time assert
     static void panic();
-    static BaseType_t getAudioPriority();
-    static BaseType_t getSysMaxPriority();
-    static BaseType_t getAppMaxPriority();
+
+    static constexpr BaseType_t getSysMaxPriority() { return configMAX_PRIORITIES - 1; } 
+    static constexpr BaseType_t getSysAvgPriority() { return (getSysMaxPriority() / 2); }
+    static constexpr BaseType_t getAudioPriority() { return (getSysAvgPriority() + 0); } // TODO: figure out what's better
+    static constexpr BaseType_t getAppMaxPriority() { return (getSysAvgPriority() - 0); } // TODO: figure out what's better
     //void collectStats();
-    
     Stats stats;
 
     FibSys(const FibSys &) = delete;
     FibSys(FibSys &&) = delete;
+
 private:
-    // can only be instantiated with FibSys::start()
+    // can only be instantiated with FibSys::boot()
     FibSys(std::uint16_t stackDepth, BaseType_t priority);
 
     // FibSys thread code
