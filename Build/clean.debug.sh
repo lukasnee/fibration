53bdf761b1709d178608d51efe9af6c59ec5d7a1
lukasnee <lukas.neverauskis@gmail.com>
2021-08-07 17:24:22 +0300

    cleanup: stepping up da build script game

--- Build/clean.debug.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BUILD_DIR=Debug
-set -e
-if [ -d "${BUILD_DIR}" ]; then
-    rm -r ${BUILD_DIR}
-fi
-
--- Build/clean.release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BUILD_DIR=Release
-set -e
-if [ -d "${BUILD_DIR}" ]; then
-    rm -r ${BUILD_DIR}
-fi
-
