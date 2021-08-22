0cfbf7553a4d819753f63e2f40eacd7a29acb6fb
lukasnee <lukas.neverauskis@gmail.com>
2021-08-22 14:52:27 +0300

    fix: flash script; add reset ST-LINK script

---
+++ Utils/reset-stlink.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+deviceNameRegex='.*ST-LINK.*'
+
+bus=$(lsusb | grep -P -o "(?<=Bus )\d{3}(?= Device \d{3}: ID [[:xdigit:]]{4}:[[:xdigit:]]{4} $deviceNameRegex)")
+device=$(lsusb | grep -P -o "(?<= Device )\d{3}(?=: ID [[:xdigit:]]{4}:[[:xdigit:]]{4} $deviceNameRegex)")
+
+devicePath="/dev/bus/usb/$bus/$device"
+
+[[ $bus == "" || $device == "" ]] && printf "not found\n" || usbreset $devicePath
+
