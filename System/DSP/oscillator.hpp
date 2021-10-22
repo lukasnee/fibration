2525ae8109ed2927535c79714d320957a0c44050
lukasnee <lukas.neverauskis@gmail.com>
2021-10-23 00:40:11 +0300

    fmt: Fib::DSP::OscillatorF32

--- System/DSP/oscillator.hpp
+++ System/DSP/oscillator.hpp
@@ -5,114 +5,119 @@
 #include "arm_math.h"
 
 #include <array>
-
-class OscillatorF32
+namespace Fib
 {
-public:
-    struct Config
+    namespace DSP
     {
-        struct Ranges
+        class OscillatorF32
         {
-            static constexpr auto amplitudeNormal = Fib::DSP::Range<float>(0.f, 1.f);
-            static constexpr auto frequencyInHz = Fib::DSP::Range<float>(0.f, 20'000.f);
-            static constexpr auto phaseInRad = Fib::DSP::Range<float>(0.f, (2.f * PI));
-        };
-    };
-
-    OscillatorF32(float sampleRateInHz = 44'100.f, float frequencyInHz = 0.f, float initialPhaseInRad = 0.f)
-        : sampleRateInHz(sampleRateInHz), frequencyInHz(frequencyInHz), phaseInRad(initialPhaseInRad){};
-    float step()
-    {
-        const float samplePeriod = 2 * PI / this->sampleRateInHz;
-        float phaseShiftInRad;
-        arm_scale_f32(&this->frequencyInHz, samplePeriod, &phaseShiftInRad, 1); /* phaseShiftInRad = f * t */
-        arm_add_f32(&this->phaseInRad, &phaseShiftInRad, &this->phaseInRad, 1); /* phaseInRad += phaseShiftInRad */
-        while (this->phaseInRad > Config::Ranges::phaseInRad.getUpper())
-        {
-            this->phaseInRad = this->phaseInRad - Config::Ranges::phaseInRad.getUpper();
-        }
-        float sinPure = arm_sin_f32(this->phaseInRad);
-        float sinAmplified;
-        arm_mult_f32(&sinPure, &this->amplitudeNormal, &sinAmplified, 1);
-        return sinAmplified;
-    }
-
-    template <std::size_t numOfSampleBlocks>
-    void step(Fib::DSP::SampleBlocks<float, numOfSampleBlocks> &sampleBlocksOut)
-    {
-        const float samplePeriod = 2 * PI / this->sampleRateInHz;
-        const float phaseDeltaInRad = this->frequencyInHz * samplePeriod;
-
-        Fib::DSP::SampleBlock<float> phaseStepsInRadSampleBlockF32 = {{1.f, 2.f, 3.f, 4.f}};
-        arm_scale_f32(phaseStepsInRadSampleBlockF32.data(), phaseDeltaInRad, phaseStepsInRadSampleBlockF32.data(), phaseStepsInRadSampleBlockF32.size());
-
-        for (std::size_t i = 0; i < sampleBlocksOut.size(); i++)
-        {
-            /* get sample phases */
-            Fib::DSP::SampleBlock<float> phaseInRadSampleBlockF32;
-            arm_offset_f32(phaseStepsInRadSampleBlockF32.data(), this->phaseInRad, phaseInRadSampleBlockF32.data(), phaseInRadSampleBlockF32.size());
-
-            /* loop phase around 2*PI and get sine values */
-            Fib::DSP::SampleBlock<float> sampleBlockF32;
-            for (std::size_t j = 0; j < phaseInRadSampleBlockF32.size(); j++)
+        public:
+            struct Config
             {
-                while (phaseInRadSampleBlockF32[j] > Config::Ranges::phaseInRad.getUpper())
+                struct Ranges
                 {
-                    phaseInRadSampleBlockF32[j] = phaseInRadSampleBlockF32[j] - Config::Ranges::phaseInRad.getUpper();
+                    static constexpr auto amplitudeNormal = Fib::DSP::Range<float>(0.f, 1.f);
+                    static constexpr auto frequencyInHz = Fib::DSP::Range<float>(0.f, 20'000.f);
+                    static constexpr auto phaseInRad = Fib::DSP::Range<float>(0.f, (2.f * PI));
+                };
+            };
+
+            OscillatorF32(float sampleRateInHz = 44'100.f, float frequencyInHz = 0.f, float initialPhaseInRad = 0.f)
+                : sampleRateInHz(sampleRateInHz), frequencyInHz(frequencyInHz), phaseInRad(initialPhaseInRad){};
+            float step()
+            {
+                const float samplePeriod = 2 * PI / this->sampleRateInHz;
+                float phaseShiftInRad;
+                arm_scale_f32(&this->frequencyInHz, samplePeriod, &phaseShiftInRad, 1); /* phaseShiftInRad = f * t */
+                arm_add_f32(&this->phaseInRad, &phaseShiftInRad, &this->phaseInRad, 1); /* phaseInRad += phaseShiftInRad */
+                while (this->phaseInRad > Config::Ranges::phaseInRad.getUpper())
+                {
+                    this->phaseInRad = this->phaseInRad - Config::Ranges::phaseInRad.getUpper();
                 }
-                sampleBlockF32[j] = arm_sin_f32(phaseInRadSampleBlockF32[j]);
+                float sinPure = arm_sin_f32(this->phaseInRad);
+                float sinAmplified;
+                arm_mult_f32(&sinPure, &this->amplitudeNormal, &sinAmplified, 1);
+                return sinAmplified;
             }
 
-            /* scale to amplitude normal */
-            arm_scale_f32(sampleBlockF32.data(), this->amplitudeNormal, sampleBlocksOut[i].data(), sampleBlocksOut[i].size());
-            this->phaseInRad = phaseInRadSampleBlockF32.back();
-        }
-    }
+            template <std::size_t numOfSampleBlocks>
+            void step(Fib::DSP::SampleBlocks<float, numOfSampleBlocks> &sampleBlocksOut)
+            {
+                const float samplePeriod = 2 * PI / this->sampleRateInHz;
+                const float phaseDeltaInRad = this->frequencyInHz * samplePeriod;
 
-    float getAmplitudeNormal() { return this->amplitudeNormal; }
-    bool setAmplitudeNormal(float amplitudeNormal)
-    {
-        bool result = false;
-        if (Config::Ranges::amplitudeNormal.isInRange(amplitudeNormal))
-        {
-            this->amplitudeNormal = amplitudeNormal;
-            result = true;
-        }
-        return result;
-    }
+                Fib::DSP::SampleBlock<float> phaseStepsInRadSampleBlockF32 = {{1.f, 2.f, 3.f, 4.f}};
+                arm_scale_f32(phaseStepsInRadSampleBlockF32.data(), phaseDeltaInRad, phaseStepsInRadSampleBlockF32.data(), phaseStepsInRadSampleBlockF32.size());
 
-    float getFrequencyInHz() { return this->frequencyInHz; }
-    bool setFrequencyInHz(float frequencyInHz)
-    {
-        bool result = false;
-        if (Config::Ranges::frequencyInHz.isInRange(frequencyInHz))
-        {
-            this->frequencyInHz = frequencyInHz;
-            result = true;
-        }
-        return result;
-    }
+                for (std::size_t i = 0; i < sampleBlocksOut.size(); i++)
+                {
+                    /* get sample phases */
+                    Fib::DSP::SampleBlock<float> phaseInRadSampleBlockF32;
+                    arm_offset_f32(phaseStepsInRadSampleBlockF32.data(), this->phaseInRad, phaseInRadSampleBlockF32.data(), phaseInRadSampleBlockF32.size());
 
-    float getPhaseInRad() { return this->phaseInRad; }
-    bool setPhaseInRad(float phaseInRad)
-    {
-        bool result = false;
-        if (Config::Ranges::phaseInRad.isInRange(phaseInRad))
-        {
-            this->phaseInRad = phaseInRad;
-            result = true;
-        }
-        return result;
-    }
+                    /* loop phase around 2*PI and get sine values */
+                    Fib::DSP::SampleBlock<float> sampleBlockF32;
+                    for (std::size_t j = 0; j < phaseInRadSampleBlockF32.size(); j++)
+                    {
+                        while (phaseInRadSampleBlockF32[j] > Config::Ranges::phaseInRad.getUpper())
+                        {
+                            phaseInRadSampleBlockF32[j] = phaseInRadSampleBlockF32[j] - Config::Ranges::phaseInRad.getUpper();
+                        }
+                        sampleBlockF32[j] = arm_sin_f32(phaseInRadSampleBlockF32[j]);
+                    }
 
-private:
-    const float sampleRateInHz = 44'100.f;
-    float amplitudeNormal = 0.f;
-    float frequencyInHz = 0.f;
-    float phaseInRad = 0.f;
-    // const OscillatorF32(OscillatorF32 &) = delete;
-    OscillatorF32(OscillatorF32 &&) = delete;
-};
+                    /* scale to amplitude normal */
+                    arm_scale_f32(sampleBlockF32.data(), this->amplitudeNormal, sampleBlocksOut[i].data(), sampleBlocksOut[i].size());
+                    this->phaseInRad = phaseInRadSampleBlockF32.back();
+                }
+            }
+
+            float getAmplitudeNormal() { return this->amplitudeNormal; }
+            bool setAmplitudeNormal(float amplitudeNormal)
+            {
+                bool result = false;
+                if (Config::Ranges::amplitudeNormal.isInRange(amplitudeNormal))
+                {
+                    this->amplitudeNormal = amplitudeNormal;
+                    result = true;
+                }
+                return result;
+            }
+
+            float getFrequencyInHz() { return this->frequencyInHz; }
+            bool setFrequencyInHz(float frequencyInHz)
+            {
+                bool result = false;
+                if (Config::Ranges::frequencyInHz.isInRange(frequencyInHz))
+                {
+                    this->frequencyInHz = frequencyInHz;
+                    result = true;
+                }
+                return result;
+            }
+
+            float getPhaseInRad() { return this->phaseInRad; }
+            bool setPhaseInRad(float phaseInRad)
+            {
+                bool result = false;
+                if (Config::Ranges::phaseInRad.isInRange(phaseInRad))
+                {
+                    this->phaseInRad = phaseInRad;
+                    result = true;
+                }
+                return result;
+            }
+
+        private:
+            const float sampleRateInHz = 44'100.f;
+            float amplitudeNormal = 0.f;
+            float frequencyInHz = 0.f;
+            float phaseInRad = 0.f;
+            // const OscillatorF32(OscillatorF32 &) = delete;
+            OscillatorF32(OscillatorF32 &&) = delete;
+        };
+    } // namespace DSP
+} // namespace Fib
 
 // class OscillatorQ31
 // {
