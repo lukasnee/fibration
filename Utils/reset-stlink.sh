af98a578e10901ec2addf76475e6fe42e3d7108a
lukasnee <lukas.neverauskis@gmail.com>
2021-11-01 00:08:22 +0200

    opt: make F5 (debug) more reliable.
    auto-force-kill all leftover/stuck st-util GDB server processes

--- Utils/reset-stlink.sh
+++ Utils/reset-stlink.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
+
+set -e 
+
+# ANSI colors
+colorReset='\033[0m'
+colorBlack='\033[0;30m'
+colorRed='\033[0;31m'
+colorGreen='\033[0;32m'
+colorYellow='\033[0;33m'
+colorBlue='\033[0;34m'
+colorPurple='\033[0;35m'
+colorCyan='\033[0;36m'
+colorWhite='\033[0;37m'
+
 deviceNameRegex='.*ST-LINK.*'
+
+# handle leftover 'st-util' (gdb server) processes
+ST_UTIL_PIDS=$(ps aux | grep -P 'st-util' | awk '{print $2}' | head -n -1 | tr '\n' ' ')
+if [[ ! -z $ST_UTIL_PIDS ]]; then
+    printf "${colorYellow}forcefully killing all 'st-util' processes, PIDS: ${colorRed}$ST_UTIL_PIDS${colorReset}\n" 
+    kill -s 15 $ST_UTIL_PIDS
+fi
 
 bus=$(lsusb | grep -P -o "(?<=Bus )\d{3}(?= Device \d{3}: ID [[:xdigit:]]{4}:[[:xdigit:]]{4} $deviceNameRegex)")
 device=$(lsusb | grep -P -o "(?<= Device )\d{3}(?=: ID [[:xdigit:]]{4}:[[:xdigit:]]{4} $deviceNameRegex)")
