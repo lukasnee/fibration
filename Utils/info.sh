838861eb4eca88a0fec4e3906ba9cf37a432eb90
lukasnee <lukas.neverauskis@gmail.com>
2021-11-01 00:08:22 +0200

    opt: make F5 (debug) more reliable.
    auto-force-kill all leftover/stuck st-util GDB server processes

--- Utils/info.sh
+++ Utils/info.sh
@@ -2,8 +2,6 @@
 
 set -e 
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
 # ANSI colors
 colorReset='\033[0m'
 colorBlack='\033[0;30m'
@@ -24,8 +22,17 @@ fi
 
 PROBE=$1
 
+# handle leftover 'st-util' (gdb server) processes
+ST_UTIL_PIDS=$(ps aux | grep -P 'st-util' | awk '{print $2}' | head -n -1 | tr '\n' ' ')
+if [[ ! -z $ST_UTIL_PIDS ]]; then
+    printf "${colorYellow}forcefully killing all 'st-util' processes, PIDS: ${colorRed}$ST_UTIL_PIDS${colorReset}\n" 
+    kill -s 15 $ST_UTIL_PIDS
+fi
+
 [[ ! -f $(command -v st-info) ]] && 
     printf "${colorRed}no 'st-info' executable\n" && exit -1
+
+printf "\n"
 
 trap "printf \"${colorRed}maybe ST-LINK is unplugged ?\n${colorReset}\"" EXIT
 if [[ $PROBE == "-p" ]]; then 
