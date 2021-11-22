efe5f684a70de4c8c484ed211131e6a668b5d99a
lukasnee <lukas.neverauskis@gmail.com>
2021-11-22 02:11:03 +0200

    feat: 4 basic oscillators demo
    SineWaveF32, SquareWaveF32, SawWaveF32, TriangeWaveF32

--- System/DSP/map.hpp
+++ System/DSP/map.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <cstdint>
-#include <limits>
-#include <array>
-#include <functional>
 #include "stm32f303xe.h"
+#include <array>
+#include <cstdint>
+#include <functional>
+#include <limits>
 extern "C"
 {
 #include "arm_math.h"
@@ -12,179 +12,195 @@ extern "C"
 
 namespace Fib
 {
-    namespace DSP
+namespace DSP
+{
+using F32 = float;
+
+/**
+ * @brief get the max value for the given bit depth
+ *
+ * @param bitDepth word width in bits from 1 to 32 !
+ * @return constexpr std::uint32_t max value
+ *
+ * @note 1-32 bitDepth max !
+ */
+template <std::uint32_t bitDepth> constexpr std::uint32_t bitDepthToMaxValue()
+{
+    return (std::numeric_limits<std::uint32_t>::max() >> (32 - bitDepth));
+}
+
+/**
+ * @brief get the center value for the given bit depth
+ *
+ * @param bitDepth
+ * @return constexpr std::uint32_t
+ */
+template <std::uint64_t bitDepth> constexpr std::uint64_t bitDepthToCenterValue()
+{
+    return (bitDepthToMaxValue<bitDepth>() / 2);
+}
+
+/**
+ * @brief  map (scale) value from [inMin inMax] relative range to [outMin outMax] relative range
+ *
+ * @param in input value to scale
+ * @param inMin input min value, the lower limit
+ * @param inMax input max value, the upper limit
+ * @param outMin output min value, the lower limit
+ * @param outMax output max value, the upper limit
+ * @return T mapped (scaled) output value
+ */
+template <typename T> T map(const T &in, const T &inMin, const T &inMax, const T &outMin, const T &outMax)
+{
+    return (in - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+/**
+ * @brief map (scale) value from [0 inMax] absolute range to [0 outMax] absolute range
+ *
+ * @param in input value to scale
+ * @param inMax input max value, the upper limit
+ * @param outMax output max value, the upper limit
+ * @return T mapped (scaled) output value
+ */
+template <typename T> T map(const T &in, const T &inMax, const T &outMax)
+{
+    return map(in, static_cast<T>(0), inMax, static_cast<T>(0), outMax);
+}
+
+constexpr q31_t floatToQ31(float in)
+{
+    return (in > 0.99999997f
+                ? std::numeric_limits<q31_t>::max()
+                : in < -1.0f ? std::numeric_limits<q31_t>::min()
+                             : static_cast<q31_t>(in * (static_cast<float>(std::numeric_limits<q31_t>::max()))));
+}
+
+/**
+ * @brief
+ *
+ * @param sample
+ * @return q31_t
+ */
+q31_t u32ToQ31(const std::uint32_t &sample);
+
+/**
+ * @brief
+ *
+ * @tparam sampleBitDepth
+ * @param sample
+ * @return q31_t
+ */
+template <std::uint32_t sampleBitDepth> q31_t sampleToQ31(const std::uint32_t &sample)
+{
     {
-        using F32 = float;
+        q31_t result;
 
-        /**
-         * @brief get the max value for the given bit depth
-         * 
-         * @param bitDepth word width in bits from 1 to 32 !
-         * @return constexpr std::uint32_t max value
-         * 
-         * @note 1-32 bitDepth max !
-         */
-        template <std::uint32_t bitDepth>
-        constexpr std::uint32_t bitDepthToMaxValue()
+        result = u32ToQ31(map<std::uint32_t>(sample, bitDepthToMaxValue<sampleBitDepth>(), bitDepthToMaxValue<32>()));
+
+        return result;
+    }
+}
+
+/**
+ * @brief
+ *
+ * @param q31
+ * @return std::uint32_t
+ */
+std::uint32_t q31ToU32(const q31_t &q31);
+
+/**
+ * @brief
+ *
+ * @tparam sampleBitDepth
+ * @param q31
+ * @return std::uint32_t
+ */
+template <std::uint32_t sampleBitDepth> std::uint32_t q31ToSample(const q31_t &q31)
+{
+    std::uint32_t result;
+
+    result = map<std::uint32_t>(q31ToU32(q31), bitDepthToMaxValue<32>(), bitDepthToMaxValue<sampleBitDepth>());
+
+    return result;
+}
+
+/**
+ * @brief swap uint32 endianness
+ *
+ * @param val
+ * @return std::uint32_t
+ */
+std::uint32_t swap(std::uint32_t val);
+
+template <typename T> class RangedValue
+{
+public:
+    constexpr RangedValue(T const &initialValue, T const &lowerLimit, T const &upperLimit,
+                          std::function<bool(T const &value)> onChangeF = nullptr)
+        : value(initialValue), lowerLimit(lowerLimit), upperLimit(upperLimit), onChangeF(onChangeF)
+    {
+    }
+    bool isInRange(T value) const
+    {
+        return (this->lowerLimit < value && value < this->upperLimit);
+    };
+
+    T get()
+    {
+        return this->value;
+    }
+
+    bool set(T value)
+    {
+        bool result = false;
+        if (this->isInRange(value))
         {
-            return (std::numeric_limits<std::uint32_t>::max() >> (32 - bitDepth));
+            this->value = value;
+            result = onChangeF ? this->onChangeF(this->value) : true;
         }
+        return result;
+    }
 
-        /**
-         * @brief get the center value for the given bit depth
-         * 
-         * @param bitDepth 
-         * @return constexpr std::uint32_t 
-         */
-        template <std::uint64_t bitDepth>
-        constexpr std::uint64_t bitDepthToCenterValue()
-        {
-            return (bitDepthToMaxValue<bitDepth>() / 2);
-        }
+    constexpr T getLowerLimit() const
+    {
+        return this->lowerLimit;
+    }
+    constexpr T getUpperLimit() const
+    {
+        return this->upperLimit;
+    }
 
-        /**
-         * @brief  map (scale) value from [inMin inMax] relative range to [outMin outMax] relative range
-         * 
-         * @param in input value to scale
-         * @param inMin input min value, the lower limit
-         * @param inMax input max value, the upper limit
-         * @param outMin output min value, the lower limit
-         * @param outMax output max value, the upper limit
-         * @return T mapped (scaled) output value
-         */
-        template <typename T>
-        T map(const T &in,
-              const T &inMin,
-              const T &inMax,
-              const T &outMin,
-              const T &outMax)
-        {
-            return (in - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
-        }
+private:
+    T value;
+    const T lowerLimit, upperLimit;
+    std::function<bool(T const &value)> onChangeF;
+};
 
-        /**
-         * @brief map (scale) value from [0 inMax] absolute range to [0 outMax] absolute range
-         * 
-         * @param in input value to scale
-         * @param inMax input max value, the upper limit
-         * @param outMax output max value, the upper limit
-         * @return T mapped (scaled) output value
-         */
-        template <typename T>
-        T map(const T &in,
-              const T &inMax,
-              const T &outMax)
-        {
-            return map(in, static_cast<T>(0), inMax, static_cast<T>(0), outMax);
-        }
+template <typename Type> using SampleBlock = std::array<Type, 4>;
+template <typename Type, std::size_t numOfBlocks> using SampleBlocks = std::array<SampleBlock<Type>, numOfBlocks>;
 
-        constexpr q31_t floatToQ31(float in)
-        {
-            return (in > 0.99999997f ? std::numeric_limits<q31_t>::max()
-                    : in < -1.0f
-                        ? std::numeric_limits<q31_t>::min()
-                        : static_cast<q31_t>(
-                              in *
-                              (static_cast<float>(std::numeric_limits<q31_t>::max()))));
-        }
+//TODO: SampleTime as class with arithmetic operands overrides (parallel value-to-value operations). Heres some scrap code:
+// template <typename Type> SampleBlock<Type> operator+(const SampleBlock<Type> &addend)
+// {
+//     SampleBlock<Type> sum;
+//     for (std::size_t i = 0; i < sum.size(); i++)
+//     {
+//         sum[i] = (*this)[i] + addend[i];
+//     }
+//     return sum;
+// }
 
-        /**
-         * @brief 
-         * 
-         * @param sample 
-         * @return q31_t 
-         */
-        q31_t u32ToQ31(const std::uint32_t &sample);
+// template <typename Type> SampleBlock<Type> operator+(const SampleBlock<Type> &first) const
+// {
+//     SampleBlock<Type> sum;
+//     for (std::size_t i = 0; i < sum.size(); i++)
+//     {
+//         sum[i] = (*this)[i] + addend[i];
+//     }
+//     return point(x + first.x, y + first.y);
+// }
 
-        /**
-         * @brief 
-         * 
-         * @tparam sampleBitDepth 
-         * @param sample 
-         * @return q31_t 
-         */
-        template <std::uint32_t sampleBitDepth>
-        q31_t sampleToQ31(const std::uint32_t &sample)
-        {
-            {
-                q31_t result;
-
-                result = u32ToQ31(map<std::uint32_t>(sample, bitDepthToMaxValue<sampleBitDepth>(), bitDepthToMaxValue<32>()));
-
-                return result;
-            }
-        }
-
-        /**
-         * @brief 
-         * 
-         * @param q31 
-         * @return std::uint32_t 
-         */
-        std::uint32_t q31ToU32(const q31_t &q31);
-
-        /**
-         * @brief 
-         * 
-         * @tparam sampleBitDepth 
-         * @param q31 
-         * @return std::uint32_t 
-         */
-        template <std::uint32_t sampleBitDepth>
-        std::uint32_t q31ToSample(const q31_t &q31)
-        {
-            std::uint32_t result;
-
-            result = map<std::uint32_t>(q31ToU32(q31), bitDepthToMaxValue<32>(), bitDepthToMaxValue<sampleBitDepth>());
-
-            return result;
-        }
-
-        /**
-         * @brief swap uint32 endianness
-         * 
-         * @param val 
-         * @return std::uint32_t 
-         */
-        std::uint32_t swap(std::uint32_t val);
-
-        template <typename T>
-        class RangedValue
-        {
-        public:
-            constexpr RangedValue(T const &initialValue, T const &lowerLimit, T const &upperLimit,
-                                  std::function<bool(T const &value)> onChangeF = nullptr)
-                : value(initialValue), lowerLimit(lowerLimit), upperLimit(upperLimit), onChangeF(onChangeF) {}
-            bool isInRange(T value) const
-            {
-                return (this->lowerLimit < value && value < this->upperLimit);
-            };
-
-            T get() { return this->value; }
-
-            bool set(T value)
-            {
-                bool result = false;
-                if (this->isInRange(value))
-                {
-                    this->value = value;
-                    result = onChangeF ? this->onChangeF(this->value) : true;
-                }
-                return result;
-            }
-
-            constexpr T getLowerLimit() const { return this->lowerLimit; }
-            constexpr T getUpperLimit() const { return this->upperLimit; }
-
-        private:
-            T value;
-            const T lowerLimit, upperLimit;
-            std::function<bool(T const &value)> onChangeF;
-        };
-
-        template <typename Type>
-        using SampleBlock = std::array<Type, 4>;
-        template <typename Type, std::size_t numOfBlocks>
-        using SampleBlocks = std::array<SampleBlock<Type>, numOfBlocks>;
-    } // namespace DSP
-} // namespace FibUtil
+} // namespace DSP
+} // namespace Fib
--- System/DSP/oscillator.hpp
+++ System/DSP/oscillator.hpp
@@ -3,46 +3,80 @@
 #include "map.hpp"
 namespace Fib::DSP::Osc
 {
-    class OscF32
+class OscF32
+{
+public:
+    struct Config
     {
-    public:
-        struct Config
+        Config(F32 sampleRateInHz, F32 frequencyInHz);
+
+        F32 sampleRateInHz = 44'100.f;
+        struct Derived
         {
-            Config(F32 sampleRateInHz, F32 frequencyInHz);
-
-            F32 sampleRateInHz = 44'100.f;
-            struct Derived
+        public:
+            Derived(F32 sampleRateInHz = 44'100.f, F32 frequencyInHz = 0.f);
+            void derive(F32 frequencyInHz);
+            const F32 &getSamplePeriodInSec() const
             {
-            public:
-                Derived(F32 sampleRateInHz = 44'100.f, F32 frequencyInHz = 0.f);
-                void derive(F32 frequencyInHz);
-                const F32 &getSamplePeriodInSec() const { return this->samplePeriodInSec; }
-                const F32 &getPhaseDeltaInRad() const { return this->phaseDeltaInRad; }
-                /* I want to return CONST reference but the flimsy CMSIS DSP API doesn't let me :') */
-                SampleBlock<F32> &getSampleBlockDeltaTimeScale() { return this->sampleBlockDeltaTimeScale; }
+                return this->samplePeriodInSec;
+            }
+            const F32 &getPhaseDeltaInRad() const
+            {
+                return this->phaseDeltaInRad;
+            }
+            /* I want to return CONST reference but the flimsy CMSIS DSP API doesn't let me :') */
+            SampleBlock<F32> &getSampleBlockDeltaTimeScale()
+            {
+                return this->sampleBlockDeltaTimeScale;
+            }
 
-            private:
-                F32 samplePeriodInSec, phaseDeltaInRad;
-                SampleBlock<F32> sampleBlockDeltaTimeScale;
-            } derived;
-        } config;
+        private:
+            F32 samplePeriodInSec, phaseDeltaInRad;
+            SampleBlock<F32> sampleBlockDeltaTimeScale;
+        } derived;
+    } config;
 
-        OscF32(F32 sampleRateInHz = 44'100.f, F32 frequencyInHz = 0.f, F32 initialPhaseInRad = 0.f);
-        virtual ~OscF32() = default;
+    OscF32(F32 sampleRateInHz = 44'100.f, F32 frequencyInHz = 0.f, F32 initialPhaseInRad = 0.f);
+    virtual ~OscF32() = default;
 
-        virtual void generate(SampleBlock<F32> &sampleBlocksOut) = 0;
+    virtual void generate(SampleBlock<F32> &sampleBlocksOut) = 0;
 
-        void generateMult(SampleBlock<F32> *pSampleBlocksOut, std::size_t sampleBlocksSize);
+    const SampleBlock<F32> &generateMult();
+    void generateMult(SampleBlock<F32> *pSampleBlocksOut, std::size_t sampleBlocksSize);
 
-        RangedValue<F32> amplitudeNormal, frequencyInHz, phaseInRad;
-    };
+    RangedValue<F32> amplitudeNormal, frequencyInHz, phaseInRad;
+};
 
-    class SineF32 : public OscF32
-    {
-    public:
-        SineF32(F32 sampleRateInHz = 44'100.f, F32 frequencyInHz = 0.f, F32 initialPhaseInRad = 0.f);
-        virtual ~SineF32() = default;
-        void generate(SampleBlock<F32> &sampleBlocksOut) override;
-    };
+class SineWaveF32 : public OscF32
+{
+public:
+    SineWaveF32(F32 sampleRateInHz = 44'100.f, F32 frequencyInHz = 0.f, F32 initialPhaseInRad = 0.f);
+    virtual ~SineWaveF32() = default;
+    void generate(SampleBlock<F32> &sampleBlocksOut) override;
+};
 
-} // Fib::DSP::Osc
+class SquareWaveF32 : public OscF32
+{
+public:
+    SquareWaveF32(F32 sampleRateInHz = 44'100.f, F32 frequencyInHz = 0.f, F32 initialPhaseInRad = 0.f);
+    virtual ~SquareWaveF32() = default;
+    void generate(SampleBlock<F32> &sampleBlocksOut) override;
+};
+
+class SawWaveF32 : public OscF32
+{
+public:
+    SawWaveF32(F32 sampleRateInHz = 44'100.f, F32 frequencyInHz = 0.f, F32 initialPhaseInRad = 0.f);
+    virtual ~SawWaveF32() = default;
+    void generate(SampleBlock<F32> &sampleBlocksOut) override;
+};
+
+class TriangeWaveF32 : public OscF32
+{
+public:
+    TriangeWaveF32(F32 sampleRateInHz = 44'100.f, F32 frequencyInHz = 0.f, F32 initialPhaseInRad = 0.f);
+    virtual ~TriangeWaveF32() = default;
+    void generate(SampleBlock<F32> &sampleBlocksOut) override;
+};
+
+} // namespace Fib::DSP::Osc
