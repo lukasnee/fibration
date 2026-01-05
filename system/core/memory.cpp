575dfd29ebf9b126fc8dc028a206700abd1b331a
Lukas Neverauskis <lukas.neverauskis@gmail.com>
2026-01-06 00:37:46 +0200

    wip: feat: dynamic memory logging

--- system/core/memory.cpp
+++ system/core/memory.cpp
@@ -1,6 +1,20 @@
-// global C++ style dynamic memory allocation override with thread-safe FreeRTOS methods
+/*
+ * Copyright (c) 2025 Lukas Neverauskis https://github.com/lukasnee
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include "ln/logger/logger.h"
+#include "ln/shell/CLI.hpp"
 #include "FreeRTOS.h"
+
 #include <cstdint>
+#include <malloc.h>
+
+LOG_MODULE(mem, LOGGER_LEVEL_NOTSET);
 
 inline void *operator new(size_t size) { return pvPortMalloc(size); }
 
@@ -13,6 +27,49 @@ inline void operator delete(void *ptr, unsigned int) { vPortFree(ptr); }
 inline void operator delete[](void *ptr) { vPortFree(ptr); }
 
 inline void operator delete[](void *ptr, unsigned int) { vPortFree(ptr); }
+
+extern "C" void *__real_malloc(size_t c);
+extern "C" void *__wrap_malloc(int c) {
+    auto p = __real_malloc(c);
+    // LOG_DEBUG("malloc(%d) = %p", c, p);
+    return p;
+}
+
+extern "C" void __real_free(void *p);
+extern "C" void __wrap_free(void *p) {
+    __real_free(p);
+    LOG_DEBUG("free(%p)", p);
+}
+
+extern "C" void *__real_calloc(size_t n, size_t c);
+extern "C" void *__wrap_calloc(int n, int c) {
+    auto p = __real_calloc(n, c);
+    LOG_DEBUG("calloc(%d, %d) = %p", n, c, p);
+    return p;
+}
+
+extern "C" void *__real_realloc(void *p, size_t c);
+extern "C" void *__wrap_realloc(void *p, int c) {
+    auto p2 = __real_realloc(p, c);
+    LOG_DEBUG("realloc(%p, %d) = %p", p, c, p2);
+    return p2;
+}
+
+namespace ln::shell {
+
+Cmd heapsize_cmd{
+    Cmd::Cfg{.name = "heapsize", .short_description = "shows current standard heap size usage", .fn = [](Cmd::Ctx ctx) {
+                 struct mallinfo info = mallinfo();
+                 ctx.cli.printf("Standard heap usage:\n");
+                 ctx.cli.printf("  Total non-mmapped bytes (arena):       %d\n", info.arena);
+                 ctx.cli.printf("  Number of free chunks (ordblks):       %d\n", info.ordblks);
+                 ctx.cli.printf("  Total allocated space (uordblks):      %d\n", info.uordblks);
+                 ctx.cli.printf("  Total free space (fordblks):           %d\n", info.fordblks);
+                 ctx.cli.printf("  Topmost releasable block (keepcost):   %d\n", info.keepcost);
+                 return Err::ok;
+             }}};
+
+} // namespace ln::shell
 
 /* relates to configSUPPORT_STATIC_ALLOCATION  */
 extern "C" void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer,
