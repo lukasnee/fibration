1e7607dcb2aae4d67d3f3646e90ec640a0314de1
lukasnee <lukas.neverauskis@gmail.com>
2026-05-03 11:36:41 +0300

    style: clang-format

--- system/core/system.hpp
+++ system/core/system.hpp
@@ -12,24 +12,6 @@
 
 class FibSys {
 public:
-    static FibSys &get_instance() {
-        static FibSys instance;
-        return instance;
-    }
-
-    static ln::lua::VM &get_lua_vm_instance() {
-        static ln::lua::VM lua_vm;
-        return lua_vm;
-    }
-
-    static ln::shell::CLI &get_cli_instance() {
-        static std::array<char, 512> input_buf;
-        static std::array<char, 512> history_buf;
-        static ln::shell::CLI instance{input_buf, history_buf};
-        instance.config.interpreter = &get_lua_vm_instance();
-        return instance;
-    }
-
     enum Priority : BaseType_t {
         lowest = 0,
         veryLow = 1,
@@ -51,7 +33,26 @@ public:
         _enumSize
     };
 
-    static_assert(Priority::_enumSize <= configMAX_PRIORITIES, "invalid priority configration!");
+    static_assert(Priority::_enumSize <= configMAX_PRIORITIES,
+                  "invalid priority configration!");
+
+    static FibSys &get_instance() {
+        static FibSys instance;
+        return instance;
+    }
+
+    static ln::lua::VM &get_lua_vm_instance() {
+        static ln::lua::VM instance;
+        return instance;
+    }
+
+    static ln::shell::CLI &get_cli_instance() {
+        static std::array<char, 512> input_buf;
+        static std::array<char, 512> history_buf;
+        static ln::shell::CLI instance{input_buf, history_buf};
+        instance.config.interpreter = &get_lua_vm_instance();
+        return instance;
+    }
 
     /**
      * @brief Launch the fibration system. This should be called in bare-metal
@@ -69,7 +70,9 @@ private:
     static constexpr size_t task_stack_size = 1 * 1024;
     struct Task : public FreeRTOS::StaticTask<task_stack_size> {
         Task(FibSys &owner)
-            : FreeRTOS::StaticTask<task_stack_size>(FibSys::Priority::highest, "FibSys"), owner(owner) {}
+            : FreeRTOS::StaticTask<task_stack_size>(FibSys::Priority::highest,
+                                                    "FibSys"),
+              owner(owner) {}
 
     private:
         virtual void taskFunction() final;
@@ -80,7 +83,9 @@ private:
     static constexpr size_t cli_svc_task_stack_size = 1024;
     struct CliSvcTask : public FreeRTOS::StaticTask<cli_svc_task_stack_size> {
         CliSvcTask(ln::shell::CLI &cli)
-            : FreeRTOS::StaticTask<cli_svc_task_stack_size>(FibSys::Priority::low, "CliSvc"), cli(cli) {}
+            : FreeRTOS::StaticTask<cli_svc_task_stack_size>(
+                  FibSys::Priority::low, "CliSvc"),
+              cli(cli) {}
 
         void start() { this->notifyGive(); }
 
